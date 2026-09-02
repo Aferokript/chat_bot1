31e0299d4ba8b926f307701c100d41e73fb44aa8
Aferokript <alberto.cardinale@bk.ru>
2026-09-02 19:08:13 +0300

    add timestamp

--- check_review_bot.py
+++ check_review_bot.py
@@ -5,50 +5,49 @@ import os
 import time
 
 
-def get_task_status(access_token):
+def get_task_status(access_token, params):
     headers = {
         "Authorization": access_token
     }
     
-    params = {}
-    
     try:
-        response = requests.get(
-            'https://dvmn.org/api/long_polling/',
-            params=params,
-            headers=headers,
-            timeout=60  
-        )
-        
-        response.raise_for_status()
-        
-        result_of_lesson = response.json()
-        
-        if result_of_lesson.get('status') == 'found':
-            reviews = result_of_lesson.get('reviews', [])
-            lesson_title = result_of_lesson.get('lesson_title', 'Неизвестный урок')
-            lesson_url = result_of_lesson.get('lesson_url', 'Ссылки нету')
-                    
-            if result_of_lesson.get('is_negative'):
-                is_fall = 'К сожалению, в работе нашлись ошибки'
+        while True:
+            response = requests.get(
+                'https://dvmn.org/api/long_polling/',
+                params=params,
+                headers=headers,
+                timeout=60
+            )
+            
+            response.raise_for_status()
+            
+            result_of_lesson = response.json()
+            
+            if result_of_lesson.get('timestamp_to_request'):
+                params['timestamp'] = result_of_lesson.get('timestamp_to_request')
+            
+            if result_of_lesson.get('status') == 'found':
+                lesson_title = result_of_lesson.get('lesson_title', 'Неизвестный урок')
+                lesson_url = result_of_lesson.get('lesson_url', 'Ссылки нету')
+                        
+                if result_of_lesson.get('is_negative'):
+                    is_fall = 'К сожалению, в работе нашлись ошибки'
+                else:
+                    is_fall = 'Преподавателю всё понравилось, можно приступать к следующему уроку!'
+                        
+                message = f"У вас проверили работу «{lesson_title}».\n{is_fall}\nСсылка: {lesson_url}"
+                return message
             else:
-                is_fall = 'Преподавателю всё понравилось, можно приступать к следующему уроку!'
-                    
-            message = f"У вас проверили работу «{lesson_title}».\n{is_fall}\nСсылка: {lesson_url}"
-            return message
-        else:
-            new_timestamp = result_of_lesson.get('last_attempt_timestamp')
-            params['timestamp'] = new_timestamp
-            return None, new_timestamp
-            
-            
+                continue
+                
+                
     except requests.exceptions.ReadTimeout:
         time.sleep(60)
+        return None
         
     except ConnectionError:
         return 'Ошибка подключения к интернету'
     
-        
 
 def bot_send_message(token, chat_id, message):
     bot = telebot.TeleBot(token)
@@ -62,10 +61,13 @@ def main():
     token = os.environ['TG_TOKEN']
     chat_id = os.environ['chat_id']
     
+    params = {}  
+    
     while True:
-        answer_about_task = get_task_status(access_token)
-        bot_send_message(token, chat_id, answer_about_task)
-    
-    
+        answer_about_task = get_task_status(access_token, params)
+        if answer_about_task:
+            bot_send_message(token, chat_id, answer_about_task)
+
+
 if __name__ == "__main__":
     main()

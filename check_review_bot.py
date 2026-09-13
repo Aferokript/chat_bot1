9e0894b917237275e2539c5fb15d7d22e932a983
Aferokript <alberto.cardinale@bk.ru>
2026-09-13 20:54:09 +0300

    catch errors and write to logs

--- check_review_bot.py
+++ check_review_bot.py
@@ -6,6 +6,22 @@ import time
 import logging
 
 
+class MyLogsHandler(logging.Handler):
+    
+    def __init__(self, token, chat_id, level=logging.NOTSET):
+        super().__init__(level=level)
+        self.token = token
+        self.chat_id = chat_id
+        self.bot = telebot.TeleBot(token)
+    
+    def emit(self, record):
+        try:
+            log_entry = self.format(record)
+            self.bot.send_message(self.chat_id, log_entry)
+        except Exception:
+            self.handleError(record)
+
+
 def get_task_status(access_token, params):
     headers = {
         "Authorization": access_token
@@ -44,7 +60,7 @@ def bot_send_message(token, chat_id, message):
     bot = telebot.TeleBot(token)
     bot.send_message(chat_id, message)
     
-
+    
 def main():
     load_dotenv()
     
@@ -52,21 +68,34 @@ def main():
     token = os.environ['TG_TOKEN']
     chat_id = os.environ['TG_CHAT_ID']
     
+    logger = logging.getLogger("review_logger")
+    logger.setLevel(logging.INFO)
+    logger.addHandler(MyLogsHandler(token, chat_id))
+    
+    logger.info('Бот запущен')
+    
     params = {}  
     
     while True:
+
         try:
             answer_about_task = get_task_status(access_token, params)
             if answer_about_task:
-                logging.debug('A new response sent to another server')
+                logger.info('A new response sent to another server')
                 bot_send_message(token, chat_id, answer_about_task)
                 
         except requests.exceptions.ReadTimeout:
             continue
             
-        except ConnectionError:
+        except requests.exceptions.ConnectionError:
+            logger.info('Ошибка соединения, бот ожидает 1 минуту')
+            time.sleep(60)
+            
+        except Exception as e:
+            logger.error(f'Бот упал с ошибкой: {e}', exc_info=True)
             time.sleep(60)
 
 
 if __name__ == "__main__":
     main()
+    
